75e821f7a131fca70abbc5c7363305c73661a2de
Oriol Puig <oriol.puig90@gmail.com>
2015-04-28 23:45:05 +0200

    Revert "Create Widgets Zones and Header&Home structure"
    This reverts commit 21150237b653f3476505168ce27372aa25ea4e6b.

--- header.php
+++ header.php
@@ -17,35 +17,33 @@
     <?php wp_head(); ?>
 </head>
 
-<body <?php body_class(); ?> >
-<div id="page" class="hfeed site container-fluid" class="">
-    <div class="row">
-        <a class="skip-link screen-reader-text" href="#content"><?php _e('Skip to content', 'mirall'); ?></a>
+<body <?php body_class(); ?>>
+<div id="page" class="hfeed site">
+    <a class="skip-link screen-reader-text" href="#content"><?php _e('Skip to content', 'mirall'); ?></a>
 
-        <div class="site-search-color"></div>
-        <header id="masthead" class="container" role="banner">
-            <div class="site-search">
-                <div class="search-engine">Cerca:<?php //get_search_form()?></div>
-                <div class="search-subscribers">Subscriptors</div>
-            </div>
-            <div class="site-branding">
-                <h1 class="site-title"><?php bloginfo('name'); ?></h1>
+    <div class="site-search-color"></div>
+    <header id="masthead" class="site-header" role="banner">
+        <div class="site-search">
+            <div class="search-engine">Cerca:<?php //get_search_form()?></div>
+            <div class="search-subscribers">Subscriptors</div>
+        </div>
+        <div class="site-branding">
+            <h1 class="site-title"><?php bloginfo('name'); ?></h1>
 
-                <h2 class="site-description"><?php bloginfo('description'); ?></h2>
-            </div>
-            <div class="site-social">
-                <p><a><i class="fa fa-facebook"></i></a> <a><i class="fa fa-twitter"></i></a> <a><i
-                            class="fa fa-google-plus-square"></i></a></p>
-            </div>
-            <nav id="site-navigation" class="main-navigation" role="navigation">
-                <button class="menu-toggle" aria-controls="primary-menu"
-                        aria-expanded="false"><?php _e('Primary Menu', 'mirall'); ?></button>
-                <?php wp_nav_menu(array('theme_location' => 'primary', 'menu_id' => 'primary-menu')); ?>
+            <h2 class="site-description"><?php bloginfo('description'); ?></h2>
+        </div>
+        <div class="site-social">
+            <p><a><i class="fa fa-facebook"></i></a> <a><i class="fa fa-twitter"></i></a> <a><i
+                        class="fa fa-google-plus-square"></i></a></p>
+        </div>
+        <nav id="site-navigation" class="main-navigation" role="navigation">
+            <button class="menu-toggle" aria-controls="primary-menu"
+                    aria-expanded="false"><?php _e('Primary Menu', 'mirall'); ?></button>
+            <?php wp_nav_menu(array('theme_location' => 'primary', 'menu_id' => 'primary-menu')); ?>
 
-            </nav>
-        </header>
-        <div class="main-navigation-color"></div>
-    </div>    
+        </nav>
+    </header>
+    <div class="main-navigation-color"></div>
     <div class="clear-content"></div>
 
-    <div id="content" class="row">
+    <div id="content" class="site-content">
